2e13da5981e90ba0c1599a8af3456f0cc8aa205a
Mali Coxon <malii.coxon@gmail.com>
2022-04-21 08:53:20 +0100

    initial commit

---
+++ app/root.tsx
@@ -0,0 +1,35 @@
+import { rootAuthLoader } from "@clerk/remix/ssr.server";
+
+export const loader: LoaderFunction = (args) => rootAuthLoader(args);
+import type { MetaFunction } from "@remix-run/cloudflare";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
